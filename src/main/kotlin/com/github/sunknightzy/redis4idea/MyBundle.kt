722691fe5fa792cae6dd8e3e91fcc8940513cd61
GitHub Action <action@github.com>
2020-12-18 20:27:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunknightzy/redis4idea/MyBundle.kt
+++ src/main/kotlin/com/github/sunknightzy/redis4idea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sunknightzy.redis4idea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sunknightzy/redis4idea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sunknightzy/redis4idea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sunknightzy.redis4idea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sunknightzy.redis4idea.MyBundle
 
 class MyApplicationService {
 
